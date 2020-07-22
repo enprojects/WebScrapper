c03f0786095fcb6fdf42fba79ccc90179b2d1ba2
en <eyaln79@gmail.com>
2020-07-22 11:36:20 +0300

    code firts implemintation

---
+++ Infrastructure/Infrastructure/migrations/20200722083137_Init.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Infrastructure.migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QueryResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SearchEngine = table.Column<string>(nullable: true),
+                    EnteredDate = table.Column<DateTime>(nullable: false),
+                    TermSearch = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QueryResults", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QueryResults");
+        }
+    }
+}
