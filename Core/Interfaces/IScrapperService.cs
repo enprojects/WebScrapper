540d68d2d164a5189e95ec5b00f8f3dac14c7054
enprojects <eyaln79@gmail.com>
2022-09-07 08:32:14 +0300

    some comment

--- Core/Interfaces/IScrapperService.cs
+++ Core/Interfaces/IScrapperService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace Core.Interfaces
 {
     public interface  IScrapperServiceManager
-    {
+    {//edited
         Task<IEnumerable<QueryResponse>> GetQueryResult(string term);
     }
 }
